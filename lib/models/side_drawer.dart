024194437dd32e550893a1e5681a886e18ccd7af
theflutterfiles <75551212+theflutterfiles@users.noreply.github.com>
2020-12-21 20:55:12 +1100

    Update and rename collapsing_navigation_drawer.dart to side_drawer.dart

--- lib/models/side_drawer.dart
+++ lib/models/side_drawer.dart
@@ -1,64 +1,58 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app_mindful_lifting/models/collapsing_list.dart';
-import 'package:flutter_app_mindful_lifting/models/navigation_model.dart';
-import 'package:flutter_app_mindful_lifting/notifiers/menu_drawer_notifier.dart';
+import 'package:flutter_app_mindful_lifting/models/custom_list_tile.dart';
+import 'package:flutter_app_mindful_lifting/models/menu_model.dart';
+import 'package:flutter_app_mindful_lifting/notifiers/menu_notifier.dart';
 import 'package:provider/provider.dart';
 
-class CollapsingNavigationDrawer extends StatefulWidget {
+class CustomSideDrawer extends StatefulWidget {
   @override
-  _CollapsingNavigationDrawerState createState() =>
-      _CollapsingNavigationDrawerState();
+  _CustomSideDrawerState createState() => _CustomSideDrawerState();
 }
 
-class _CollapsingNavigationDrawerState
-    extends State<CollapsingNavigationDrawer> {
-  
-  //1
-  int currentSelectedIndex;
+int currentSelectedIndex;
 
+class _CustomSideDrawerState extends State<CustomSideDrawer> {
   @override
   void initState() {
+    MenuDrawerNotifier menuDrawerNotifier =
+        Provider.of<MenuDrawerNotifier>(context, listen: false);
+    currentSelectedIndex = menuDrawerNotifier.getCurrentDrawer;
     super.initState();
-    MenuDrawerNorifier menuDrawerNorifier =
-        Provider.of<MenuDrawerNorifier>(context, listen: false);
-    currentSelectedIndex = menuDrawerNorifier.getCurrentDrawer;
   }
 
   @override
   Widget build(BuildContext context) {
-    var drawer = Provider.of<MenuDrawerNorifier>(context, listen: false);
+    var drawer = Provider.of<MenuDrawerNotifier>(context, listen: false);
 
-   //2
     return Container(
       width: 250,
       color: Colors.blue,
-      child: ListView.builder( //3
+      child: ListView.builder(
         itemBuilder: (context, index) {
-          return CollapsingListTile(
+          return CustomListTile(
             onTap: () {
               switch (index) {
                 case 0:
                   drawer.setCurrentDrawer(0);
-                  //4
                   Navigator.popAndPushNamed(context, "home");
                   break;
+
                 case 1:
                   drawer.setCurrentDrawer(1);
                   Navigator.popAndPushNamed(context, "tasks");
                   break;
+
                 case 2:
                   drawer.setCurrentDrawer(2);
                   Navigator.popAndPushNamed(context, "projects");
                   break;
               }
             },
-            //5
             title: navigationItems[index].title,
             isSelected: currentSelectedIndex == index,
             icon: navigationItems[index].icon,
           );
         },
-        //
         itemCount: navigationItems.length,
       ),
     );
