75348bebaea9d8530cbb7e3d4452c8b1e8874367
theflutterfiles <75551212+theflutterfiles@users.noreply.github.com>
2020-12-21 20:20:36 +1100

    Update navigation_model.dart

--- lib/models/navigation_model.dart
+++ lib/models/navigation_model.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class NavigationModel {
+
   String title;
   IconData icon;
   Action onNavigationSelection;
@@ -10,10 +11,6 @@ class NavigationModel {
 
 List<NavigationModel> navigationItems = [
   NavigationModel(icon: Icons.home_rounded, title: "Home"),
-  NavigationModel(icon: Icons.dashboard_rounded, title: "Dashboard"),
-  NavigationModel(icon: Icons.info_rounded, title: "About"),
-  NavigationModel(icon: Icons.monetization_on_rounded, title: "Expenses"),
-  NavigationModel(icon: Icons.person_pin, title: "Collab"),
-  NavigationModel(icon: Icons.check_circle, title: "Tasks"),
-  NavigationModel(icon: Icons.date_range_rounded, title: "Events"),
+  NavigationModel(icon: Icons.dashboard_rounded, title: "Tasks"),
+  NavigationModel(icon: Icons.info_rounded, title: "Projects"),
 ];
